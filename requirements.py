4f3f1f7c85e11e04a9ae9f1f7f5ccb7f45b682d5
Aaron <34774965+Assassinsarms@users.noreply.github.com>
2021-04-11 12:49:56 +0100

    Added requirements.txt

---
+++ requirements.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+np.random.seed(42)
+import time
+import sys
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+import pickle
+from math import ceil
+import random
+import sys
+import torch 
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+torch.set_printoptions(edgeitems=10)
